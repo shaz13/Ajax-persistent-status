c2f75f7a803997af0387d1f765517e27627916fe
Shaz <shahebaz13@gmail.com>
2016-12-10 19:11:55 +0530

    Add files via upload

---
+++ persist-stat.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+	setInterval(function() {
+  $.ajax({
+    type: "GET",
+    url: "https://www.example.org/",
+    dataType: "script",
+
+    success: function(data) {
+
+      $("#box").fadeIn(700);
+			    $('#ia').css("color","#4caf50");
+			  $('#box').css("background-color","#99cbe6");
+			$('#box').html("<div class=text>Synchronising Data</div>");
+      $('#status').html("<b>Online</b>");
+      $('#toast').css("background","#4caf50");
+
+    },
+    error: function() {
+
+      $("#box").fadeIn(700);
+			  $('#ia').css("color","#d60f0f");
+			$('#box').css("background-color","#bab8bb");
+		$('#box').html("<div class=text>Connection lost</div>");
+       $('#status').html("<b>Offline</b>");
+			 $('#scrl').html("<b>It Works!</b>");
+       $('#toast').css("background","#d60f0f");
+
+    }
+  });
+
+}, 90 * 10);
+
+});
