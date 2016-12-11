b49fe3f3daae0ac2dec17cbc24e097feafde3469
Shaz <shahebaz13@gmail.com>
2016-12-11 18:31:09 +0530

    Update persist-stat.js

--- persist-stat.js
+++ persist-stat.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+ $('#scrl').hide();
 
 	setInterval(function() {
   $.ajax({
@@ -8,22 +9,26 @@ $(document).ready(function(){
 
     success: function(data) {
 
-      $("#box").fadeIn(700);
+      $("#box").fadeIn(900);
+
 			    $('#ia').css("color","#4caf50");
 			  $('#box').css("background-color","#99cbe6");
 			$('#box').html("<div class=text>User is Online</div>");
       $('#status').html("<b>Online</b>");
       $('#toast').css("background","#4caf50");
+				$("#scrl").fadeIn(2400);
 
     },
     error: function() {
 
-      $("#box").fadeIn(700);
+      $("#box").fadeIn(900);
+			$("#scrl").fadeIn(2400);
 			  $('#ia').css("color","#d60f0f");
 			$('#box').css("background-color","#bab8bb");
 		$('#box').html("<div class=text>Connection lost</div>");
        $('#status').html("<b>Offline</b>");
 			 $('#scrl').html("<b>It Works!</b>");
+			  $('#scrl').css("left", "28%");
        $('#toast').css("background","#d60f0f");
 
     }
